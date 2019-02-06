1e9fe803142f91e27e5591448eb006217f34b8e4
Harshavardhanan <harshavardhan2990@gmail.com>
2019-02-06 10:09:12 +0530

    Basic Express App Setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,25 @@
 import express from 'express';
+import bodyparser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3001);
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cors());
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello');
+});
+
+app.get('/favicon.ico', function (req, res) {
+    res.sendStatus(204);
+});
+
+app.listen(process.env.PORT, () => console.log('Started listening at port 3001'));
