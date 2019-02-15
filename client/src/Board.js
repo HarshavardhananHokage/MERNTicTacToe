226b94dd5a4eb71826cbf37c6d287e4cf54ca978
Harshavardhanan <harshavardhan2990@gmail.com>
2019-02-15 09:14:03 +0530

    Block modification if box filled

--- client/src/Board.js
+++ client/src/Board.js
@@ -3,24 +3,33 @@ import { connect } from 'react-redux';
 import { updateGameMoveList } from './redux/actions';
 
 function Board(props) {
+
+    function onClickPlayer(id) {
+        if ( props.currMap[id] === null ) {
+            props.onClickBox(id);
+        } else {
+            alert("Not allowed!");
+        }
+    }
+
     return (
         <div className="centerflex">
             <table>
                 <tbody>
                     <tr>
-                        <td onClick={ () => props.onClickBox(0)}>{ props.currMap[0] }</td>
-                        <td onClick={ () => props.onClickBox(1)}>{ props.currMap[1] }</td>
-                        <td onClick={ () => props.onClickBox(2)}>{ props.currMap[2] }</td>
+                        <td onClick={ () => onClickPlayer(0)}>{ props.currMap[0] }</td>
+                        <td onClick={ () => onClickPlayer(1)}>{ props.currMap[1] }</td>
+                        <td onClick={ () => onClickPlayer(2)}>{ props.currMap[2] }</td>
                     </tr>
                     <tr>
-                        <td onClick={ () => props.onClickBox(3)}>{ props.currMap[3] }</td>
-                        <td onClick={ () => props.onClickBox(4)}>{ props.currMap[4] }</td>
-                        <td onClick={ () => props.onClickBox(5)}>{ props.currMap[5] }</td>
+                        <td onClick={ () => onClickPlayer(3)}>{ props.currMap[3] }</td>
+                        <td onClick={ () => onClickPlayer(4)}>{ props.currMap[4] }</td>
+                        <td onClick={ () => onClickPlayer(5)}>{ props.currMap[5] }</td>
                     </tr>
                     <tr>
-                        <td onClick={ () => props.onClickBox(6)}>{ props.currMap[6] }</td>
-                        <td onClick={ () => props.onClickBox(7)}>{ props.currMap[7] }</td>
-                        <td onClick={ () => props.onClickBox(8)}>{ props.currMap[8] }</td>
+                        <td onClick={ () => onClickPlayer(6)}>{ props.currMap[6] }</td>
+                        <td onClick={ () => onClickPlayer(7)}>{ props.currMap[7] }</td>
+                        <td onClick={ () => onClickPlayer(8)}>{ props.currMap[8] }</td>
                     </tr>
                 </tbody>
             </table>
